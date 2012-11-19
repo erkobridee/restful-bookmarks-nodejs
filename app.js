788601ff3d013765cf6127a3efa71cff3df32802
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-11-19 17:30:38 -0200

    define directories locations using path

--- app.js
+++ app.js
@@ -16,14 +16,17 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
+  app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(require('stylus').middleware({
+    src: path.join(__dirname, 'views'),
+    dest: path.join(__dirname, 'public')
+  }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
