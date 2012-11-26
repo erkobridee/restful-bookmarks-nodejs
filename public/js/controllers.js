3d22fd51f2883a37595ec6ec1eb1b381bf815874
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-11-26 16:22:44 -0200

    ui pagination control

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,28 +3,80 @@ Ctrl = (function() {
 
   function Ctrl() {}
 
+  Ctrl.prototype.Root = function($scope) {
+
+    $scope.metainf = {
+      pageSize: 2,
+      count: 0,
+      lastPage: 0,
+      lastPageSize: 0,
+      totalPages: 0
+    };
+
+    //---
+
+    $scope.getPageSize = function() {
+      return $scope.metainf.pageSize;      
+    }
+
+    //---
+
+    $scope.setLastPage = function(value) {
+      $scope.metainf.lastPage = value;
+    }
+
+    $scope.getLastPage = function() {
+      return $scope.metainf.lastPage; 
+    }
+
+    //---
+
+    $scope.updateMetainf = function(count, lastPageSize, totalPages) {
+      $scope.metainf.count = count;
+      $scope.metainf.lastPageSize = lastPageSize;
+      $scope.metainf.totalPages = totalPages;
+    }
+
+    //--- 
+
+    $scope.updatePageIndex = function() {
+      var mod = $scope.metainf.count % $scope.metainf.pageSize;
+      
+      $scope.metainf.lastPage = $scope.metainf.totalPages;
+      if(mod !== 0) {
+        $scope.metainf.lastPage--;
+      } 
+    }
+
+  }
+
   Ctrl.prototype.List = function($scope, BookmarkResource) {
     
-    $scope.pageSize = 2;
-
     function loadData(page) {
       BookmarkResource.get(
         {
           page: page,
-          size: $scope.pageSize
+          size: $scope.getPageSize()
         },
         function(res) {
           $scope.bookmarks = res;
+
+          $scope.updateMetainf(
+            res.count,
+            res.data.length,
+            res.pages
+          );
       });
     }
 
     $scope.setPage = function() {
       if(this.n != $scope.bookmarks.page) {
-        loadData(this.n);
+        $scope.setLastPage(this.n);
+        loadData($scope.getLastPage());
       }
     }
 
-    loadData(0);
+    loadData($scope.getLastPage());
   }
 
   Ctrl.prototype.New = function($scope, $location, BookmarkResource) {
@@ -37,6 +89,7 @@ Ctrl = (function() {
 	  	console.log('save button');
 	  	
 	  	$scope.bookmark.$save(function(res) {
+        $scope.updatePageIndex();
 	  		$location.path('/');
 	  	});
 	  }
