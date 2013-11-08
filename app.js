85193b8336dfca1674151b3bb1038f8299303a13
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-11-08 11:30:31 -0200

    comment unused

--- app.js
+++ app.js
@@ -16,17 +16,19 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'app', 'views'));
-  app.set('view engine', 'jade');
+  //app.set('views', path.join(__dirname, 'app', 'views'));
+  //app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  /*
   app.use(require('stylus').middleware({
     src: path.join(__dirname, 'app', 'views'),
     dest: path.join(__dirname, 'public')
   }));
+  */
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
